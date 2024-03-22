0641be468169c26feefa32989dd51c62b4b4ab1b
Norcore <norbertrichter94@gmail.com>
2024-03-22 19:34:39 +0100

    debug: couldn't run outbound network cmds inside ec2. extended sg with egress

--- infra/aws_sg.tf
+++ infra/aws_sg.tf
@@ -10,4 +10,11 @@ resource "aws_security_group" "ec2_security_group_rnorbi" {
     cidr_blocks = ["84.0.255.119/32"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  // Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"]  // Allow outbound traffic to all destinations
+  }
+
 }
