14275e78bffb3901dcb3c1d0e2a2c5b67dc06285
Norcore <norbertrichter94@gmail.com>
2024-03-22 20:47:29 +0100

    bugfix: extended dependencies script to include AcceptEnv in ssh config

--- dependencies.sh
+++ dependencies.sh
@@ -8,5 +8,11 @@ sudo service docker start
 # Add your user to the docker group to run Docker commands without sudo
 sudo usermod -aG docker $USER
 
+# Ensure that SSH server is configured to accept GITHUB_RUN_NUMBER environment variable
+sudo echo "AcceptEnv GITHUB_RUN_NUMBER" >> /etc/ssh/sshd_config
+
+# Restart SSH service to apply the changes
+sudo systemctl restart sshd
+
 # Log out and log back in to apply the group membership changes
 exit
