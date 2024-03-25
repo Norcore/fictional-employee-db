729911774db3edcbcfdddd0dc50ea3be05999d39
Norcore <norbertrichter94@gmail.com>
2024-03-25 16:15:55 +0100

    debugging github_run_num related docker error

--- dependencies.sh
+++ dependencies.sh
@@ -8,14 +8,8 @@ sudo service docker start
 # Add your user to the docker group to run Docker commands without sudo
 sudo usermod -aG docker $USER
 
-# Debugging: Print out the values of environment variables
-echo "MONGO_URI: ${MONGO_URI}"
-echo "REACT_APP_FRONTEND_URL: ${REACT_APP_FRONTEND_URL}"
-echo "REACT_APP_BACKEND_URL: ${REACT_APP_BACKEND_URL}"
-echo "GITHUB_RUN_NUM: ${GITHUB_RUN_NUM}"
-
 # Ensure that SSH server is configured to accept GITHUB_RUN_NUMBER environment variable
-sudo sh -c 'echo "AcceptEnv MONGO_URI REACT_APP_FRONTEND_URL REACT_APP_BACKEND_URL" >> /etc/ssh/sshd_config'
+sudo sh -c 'echo "AcceptEnv GITHUB_RUN_NUM MONGO_URI REACT_APP_FRONTEND_URL REACT_APP_BACKEND_URL" >> /etc/ssh/sshd_config'
 
 # Restart SSH service to apply the changes
 sudo systemctl restart sshd
